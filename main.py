c0aee85d7d99438aab10704426b49ac1abf4175d
Shital Shah <shitals@microsoft.com>
2020-01-08 22:44:07 -0800

    added ranking

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import pathlib
 in_str = input('Enter words or parts: ')
 tokens = in_str.split()
 
-words = pathlib.Path('words.txt').read_text().splitlines()
+words = set(pathlib.Path('words.txt').read_text().splitlines())
 found = []
 for token in tokens:
     for word in words:
-        if word.endswith(token) or word.startswith(token):
-            found.append(word)
+        if len(word)>2 and (word.endswith(token) or word.startswith(token)):
+            found.append((word, len(word)))
 
-found.sort(key=lambda w: len(w))
-
-print(found)
+found.sort(key=lambda t: t[1])
+for f in found:
+    print(f)
